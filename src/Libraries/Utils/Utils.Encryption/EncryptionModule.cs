7379c080add1db9803359938958db558f75a09d6
SaveApis | Felix Hake <felix.hake@saveapis.com>
2025-09-18 21:58:41 +0200

    feat: Create encryption module
    Author: Felix Hake

---
+++ src/Libraries/Utils/Utils.Encryption/EncryptionModule.cs
@@ -0,0 +1,33 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Utils.Encryption.Domain.Options;
+using Utils.Encryption.Infrastructure.Services.Encryption;
+using Utils.Encryption.Infrastructure.Services.Hashing;
+
+namespace Utils.Encryption;
+
+public class EncryptionModule(IConfiguration configuration) : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        RegisterOptions(builder, configuration);
+        
+        builder.RegisterType<EncryptionService>()
+            .As<IEncryptionService>()
+            .InstancePerLifetimeScope();
+        builder.RegisterType<HashingService>()
+            .As<IHashingService>()
+            .InstancePerLifetimeScope();
+    }
+
+    private static void RegisterOptions(ContainerBuilder builder, IConfiguration configuration)
+    {
+        var collection = new ServiceCollection();
+
+        collection.Configure<EncryptionOption>(configuration.GetSection("Encryption"));
+
+        builder.Populate(collection);
+    }
+}
