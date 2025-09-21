acffc7b5203e4a3c110c7b4c2dd478d64a138465
SaveApis | Felix Hake <felix.hake@saveapis.com>
2025-09-21 17:20:14 +0200

    feat: Add core/write db context
    Author: Felix Hake

---
+++ src/Software/Software.Middleware/Domains/Common/Persistence/Sql/Context/CoreReadDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Software.Middleware.Domains.Application.Domain.Models.Entities;
+using Software.Middleware.Domains.Application.Persistence.Sql.Configurations;
+using Utils.EntityFrameworkCore.Infrastructure.Context.Base;
+
+namespace Software.Middleware.Domains.Common.Persistence.Sql.Context;
+
+public class CoreReadDbContext(DbContextOptions<CoreReadDbContext> options) : BaseReadDbContext<CoreReadDbContext>(options)
+{
+    protected override string Schema => "Core";
+    
+    public IQueryable<ApplicationEntity> Applications => Set<ApplicationEntity>().AsNoTracking();
+    public IQueryable<ApplicationScopeEntity> ApplicationScopes => Set<ApplicationScopeEntity>().AsNoTracking();
+
+    protected override void RegisterConfigurations(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ApplicationEntityTypeConfiguration());
+        modelBuilder.ApplyConfiguration(new ApplicationScopeEntityTypeConfiguration());
+    }
+}
---
+++ src/Software/Software.Middleware/Domains/Common/Persistence/Sql/Context/CoreWriteDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Software.Middleware.Domains.Application.Domain.Models.Entities;
+using Software.Middleware.Domains.Application.Persistence.Sql.Configurations;
+using Utils.EntityFrameworkCore.Infrastructure.Context.Base;
+
+namespace Software.Middleware.Domains.Common.Persistence.Sql.Context;
+
+public class CoreWriteDbContext(DbContextOptions<CoreWriteDbContext> options) : BaseWriteDbContext<CoreWriteDbContext>(options)
+{
+    protected override string Schema => "Core";
+    
+    public DbSet<ApplicationEntity> Applications => Set<ApplicationEntity>();
+    public DbSet<ApplicationScopeEntity> ApplicationScopes => Set<ApplicationScopeEntity>();
+
+    protected override void RegisterConfigurations(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ApplicationEntityTypeConfiguration());
+        modelBuilder.ApplyConfiguration(new ApplicationScopeEntityTypeConfiguration());
+    }
+}
