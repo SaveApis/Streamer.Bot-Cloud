d4cd101df11c99292615c58a21000195ddb19905
SaveApis | Felix Hake <felix.hake@saveapis.com>
2025-10-13 20:46:47 +0200

    feat(core): Add registration extension
    Author: Felix Hake

---
+++ src/Libraries/Software.Core/Application/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Util.Core;
+using Util.Core.Application.Helpers;
+
+namespace Software.Core.Application.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static void UseAutofac(this WebApplicationBuilder builder, Assembly softwareAssembly)
+    {
+        var thisAssembly = Assembly.GetExecutingAssembly();
+        var utilAssemblies = GetUtilAssemblies();
+
+        var assemblyHelper = new AssemblyHelper([softwareAssembly, thisAssembly, ..utilAssemblies]);
+        builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+            .ConfigureContainer<ContainerBuilder>((context, containerBuilder) => containerBuilder.RegisterModule(new CoreModule(assemblyHelper)));
+    }
+
+    private static List<Assembly> GetUtilAssemblies()
+    {
+        return new List<Assembly>();
+    }
+}
