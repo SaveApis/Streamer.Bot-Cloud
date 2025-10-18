ccf8b8bb149604ccb81c655d3c5e3e48c5750076
SaveApis | Felix Hake <felix.hake@saveapis.com>
2025-10-18 14:26:56 +0200

    chore: Setup projects
    Author: Felix Hake

---
+++ src/Libraries/Software.Core/Application/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Utils.Core;
+using Utils.Core.Application.Helpers;
+
+namespace Software.Core.Application.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static void UseAutofac(this WebApplicationBuilder builder, Assembly softwareAssembly, Action<IAssemblyHelper, HostBuilderContext, ContainerBuilder>? registerSoftwareModules = null)
+    {
+        var assemblyHelper = new AssemblyHelper(Assembly.GetExecutingAssembly(), softwareAssembly);
+
+        builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+            .ConfigureContainer<ContainerBuilder>((context, containerBuilder) =>
+                {
+                    // Utils
+                    containerBuilder.RegisterModule(new CoreModule(assemblyHelper));
+
+                    // Software
+                    registerSoftwareModules?.Invoke(assemblyHelper, context, containerBuilder);
+                }
+            );
+    }
+}
